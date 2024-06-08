91a53f6ab60f4fb34641cad7437441478af85e50
so3659 <so3659@naver.com>
2024-06-08 19:29:36 +0900

    Add WebView on MainPage #2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:bigdata_project/caterory.dart';
+import 'package:bigdata_project/inflearn.dart';
+import 'package:bigdata_project/udemy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -36,139 +38,123 @@ class _HomepageState extends State<Homepage> {
   late final WebViewController _controller;
 
   @override
-  void initState() {
-    super.initState();
-
-    // #docregion platform_features
-    late final PlatformWebViewControllerCreationParams params;
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-
-    final WebViewController controller =
-        WebViewController.fromPlatformCreationParams(params);
-    // #enddocregion platform_features
-
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            debugPrint('WebView is loading (progress : $progress%)');
-          },
-          onPageStarted: (String url) {
-            debugPrint('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            debugPrint('Page finished loading: $url');
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('''
-Page resource error:
-  code: ${error.errorCode}
-  description: ${error.description}
-  errorType: ${error.errorType}
-  isForMainFrame: ${error.isForMainFrame}
-          ''');
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              debugPrint('blocking navigation to ${request.url}');
-              return NavigationDecision.prevent;
-            }
-            debugPrint('allowing navigation to ${request.url}');
-            return NavigationDecision.navigate;
-          },
-          onHttpError: (HttpResponseError error) {
-            debugPrint('Error occurred on page: ${error.response?.statusCode}');
-          },
-          onUrlChange: (UrlChange change) {
-            debugPrint('url change to ${change.url}');
-          },
-        ),
-      )
-      ..addJavaScriptChannel(
-        'Toaster',
-        onMessageReceived: (JavaScriptMessage message) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(message.message)),
-          );
-        },
-      )
-      ..loadRequest(Uri.parse('https://www.inflearn.com'));
-
-    // #docregion platform_features
-    if (controller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-      (controller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
-    }
-    // #enddocregion platform_features
-
-    _controller = controller;
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return WillPopScope(
-      onWillPop: () {
-        var future = _controller.canGoBack();
-        future.then((canGoBack) => {
-              if (canGoBack)
-                {_controller.goBack()}
-              else
-                {
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Text('앱 종료'),
-                      content: const Text('정말로 종료하시겠어요? 🥺'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('아니오'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            SystemNavigator.pop();
-                          },
-                          child: const Text('예'),
-                        ),
-                      ],
-                    ),
-                  )
-                }
-            });
-        return Future.value(false);
-      },
-      child: SafeArea(
-        child: Scaffold(
-            appBar: AppBar(
+      onWillPop: () async {
+        if (Navigator.of(context).canPop()) {
+          Navigator.of(context).pop();
+          return Future.value(false);
+        } else {
+          bool? exitApp = await showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('앱 종료'),
+              content: const Text('정말로 종료하시겠어요? 🥺'),
               actions: [
                 TextButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const CatalogScreen()),
-                    );
+                    Navigator.of(context).pop(false);
                   },
-                  child: const Text(
-                    '어떤 직무를 찾으세요?',
-                    style: TextStyle(color: Colors.black),
+                  child: const Text('아니오'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    SystemNavigator.pop();
+                  },
+                  child: const Text('예'),
+                ),
+              ],
+            ),
+          );
+          return exitApp ?? false;
+        }
+      },
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CatalogScreen()),
+                  );
+                },
+                child: const Text(
+                  '나에게 맞는 직무는?',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  margin:
+                      EdgeInsets.fromLTRB(10, screenSize.height * 0.05, 10, 20),
+                  child: SizedBox(
+                    height: screenSize.height * 0.35, // 전체 화면 높이의 25%를 높이로 설정
+                    width: screenSize.width * 0.7, // 전체 화면 너비의 40%를 너비로 설정
+                    child: FloatingActionButton(
+                      heroTag: 'inflearn',
+                      backgroundColor: Colors.white,
+                      onPressed: () async {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Inflearn()),
+                        );
+                      },
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(
+                            image: AssetImage('assets/inflearn_logo.png'),
+                            height: 150,
+                            width: 150,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.fromLTRB(10, screenSize.height * 0.05, 10, 20),
+                  child: SizedBox(
+                    height: screenSize.height * 0.35, // 전체 화면 높이의 25%를 높이로 설정
+                    width: screenSize.width * 0.7, // 전체 화면 너비의 40%를 너비로 설정
+                    child: FloatingActionButton(
+                      heroTag: 'udemy',
+                      backgroundColor: Colors.white,
+                      onPressed: () async {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Udemy()),
+                        );
+                      },
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(
+                            image: AssetImage('assets/Udemy_logo.png'),
+                            height: 150,
+                            width: 150,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ],
             ),
-            body: SafeArea(child: WebViewWidget(controller: _controller))),
+          ),
+        ),
       ),
     );
   }
