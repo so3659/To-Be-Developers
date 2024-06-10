660550d00aa1f69edcc914b6212f305d1bce4991
Chun Hyunwoo <1000chw@gmail.com>
2024-06-10 16:42:59 +0900

    style: lectures api 코드 일부 수정

--- source/lectures/lecturesController.js
+++ source/lectures/lecturesController.js
@@ -4,7 +4,6 @@ import lecturesProvider from "./lecturesProvider";
 import lecturesService from "./lecturesService";
 
 const lecturesController = {
-
 };
 
 export default lecturesController;
--- source/lectures/lecturesRouter.js
+++ source/lectures/lecturesRouter.js
@@ -3,6 +3,8 @@ import lecturesController from "./lecturesController";
 
 const lecturesRouter = express.Router();
 
-lecturesRouter.get("/");
+lecturesRouter.get("/", (req, res) => {
+    console.log("good!")});
+
 
 export default lecturesRouter;
