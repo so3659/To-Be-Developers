23eba7caecb4f5043882b884a53b84d7a84db4f8
Chun Hyunwoo <1000chw@gmail.com>
2024-06-10 17:06:51 +0900

    feat: selectLangRank 추가

--- source/developers/devsDao.js
+++ source/developers/devsDao.js
@@ -9,6 +9,19 @@ const devsDao = {
             return {error: true, message: e.message};
         }
     },
+
+    selectLangRank: async (devType, role, connection) => {
+        const sql = 'select `rank`, language, `count` from languageRank where role = ? and develop_id = ? order by `rank` asc limit 5';
+        try {
+            const [result] = await connection.query(sql, [role, Number(devType)]);
+            return result;
+        } catch (e) {
+            console.log(e);
+            return {error: true, message: e.message};
+        }
+    },
+
+
 };
 
 export default devsDao;
