4a3f651292c082ed20f90645ac6944a90e507bcc
Chun Hyunwoo <1000chw@gmail.com>
2024-06-10 17:04:36 +0900

    feat: selectEditorRank 추가

--- source/developers/devsDao.js
+++ source/developers/devsDao.js
@@ -1,6 +1,13 @@
 const devsDao = {
-    selectReportInfo: async (devType, role, connection) => {
-        return {"res": "good!"}
+    selectEditorRank: async (devType, role, connection) => {
+        const sql = 'select `rank`, editor, `count` from editorRank where role = ? and develop_id = ? order by `rank` asc limit 5';
+        try {
+            const [result] = await connection.query(sql, [role, Number(devType)]);
+            return result;
+        } catch (e) {
+            console.log(e);
+            return {error: true, message: e.message};
+        }
     },
 };
 
