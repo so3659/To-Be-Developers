d494ecc1aef7645ecb5b8a6957e0eacd681b24d2
Chun Hyunwoo <1000chw@gmail.com>
2024-06-13 14:21:51 +0900

    feat: lectures api Append

--- source/lectures/lecturesProvider.js
+++ source/lectures/lecturesProvider.js
@@ -2,6 +2,24 @@ import pool from "../../config/database";
 import lecturesDao from "./lecturesDao";
 
 const lecturesProvider = {
+    getRandomLectures: async () => {
+        try {
+            const connection = await pool.getConnection();
+            const lectures = await lecturesDao.selectLectures(connection);
+            for (const lecture of lectures) {
+                if ("inflearn" === lecture.image_url){
+                    lecture.link_url = "https://inflearn.com" + lecture.link_url;
+                }
+                else {
+                    lecture.link_url = "https://udemy.com" + lecture.link_url;
+                }
+
+            }
+            return lectures;
+        } catch (e) {
+            return {error: true, message: e.message};
+        }
+    }
 
 };
 
