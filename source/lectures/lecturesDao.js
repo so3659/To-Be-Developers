0eb3c812add054703c00676eb2e903158b07a398
Chun Hyunwoo <1000chw@gmail.com>
2024-06-13 14:21:51 +0900

    feat: lectures api Append

--- source/lectures/lecturesDao.js
+++ source/lectures/lecturesDao.js
@@ -1,5 +1,14 @@
 const lecturesDao = {
-
+    selectLectures: async (connection) => {
+        const sql = 'select * from lectures order by rand() limit 10';
+        try {
+            const [result] = await connection.query(sql);
+            return result;
+        } catch (e) {
+            console.log(e);
+            return {error: true, message: e.message};
+        }
+    }
 };
 
 export default lecturesDao;
--- source/lectures/lecturesRouter.js
+++ source/lectures/lecturesRouter.js
@@ -3,8 +3,7 @@ import lecturesController from "./lecturesController";
 
 const lecturesRouter = express.Router();
 
-lecturesRouter.get("/", (req, res) => {
-    console.log("good!")});
+lecturesRouter.get("/recommend", lecturesController.getRandomLectures);
 
 
 export default lecturesRouter;
